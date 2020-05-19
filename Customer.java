05335f795033894537ec008435866f3d95645599
Priyank Patel <pbpatel23796@gmail.com>
2020-05-18 22:14:40 -0400

    Step 0
    Source code downloaded, JUnit library added and code beautified

---
+++ Customer.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+public class Customer {
+	
+    private String name;
+    private List rentals = new ArrayList();
+
+    public Customer(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void addRental(Rental arg) {
+        rentals.add(arg);
+    }
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Iterator rentalIterator = rentals.iterator();
+        String result = "Rentals: " + getName() + "\n";
+        while (rentalIterator.hasNext()) {
+            double thisAmount = 0;
+            Rental each = (Rental) rentalIterator.next();
+
+            //determine amounts for each line
+
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                each.getDaysRented() > 1) frequentRenterPoints++;
+
+            //show figures for this rental
+            result += each.getDaysRented() +
+                " days of '" + each.getMovie().getTitle() +
+                "' $" + String.valueOf(thisAmount) + "\n";
+            totalAmount += thisAmount;
+        }
+        //add footer lines
+        result += "Total = $" + totalAmount + "\n";
+        result += "Frequent renter points = " + frequentRenterPoints + "\n";
+        return result + "---\n";
+    }
+
+}
---
+++ TestMovieRental.java
@@ -0,0 +1,48 @@
+import junit.framework.*;
+
+public class TestMovieRental extends TestCase {
+	
+    private Rental rentMatrix, rentMatrix2;
+    private Customer customer;
+
+    public void setUp() {
+        rentMatrix = new Rental(new Movie("Matrix", Movie.REGULAR), 4);
+        rentMatrix2 = new Rental(new Movie("Matrix2", Movie.NEW_RELEASE), 5);
+        customer = new Customer("John Hood");
+    }
+    
+    public void testInitialCustomer() {
+        assertEquals("Rentals: John Hood\n" +
+            "Total = $0.0\n" +
+            "Frequent renter points = 0\n" +
+            "---\n",
+            customer.statement());
+    }
+    
+    public void testRentingCustomer() {
+        customer.addRental(rentMatrix);
+        customer.addRental(rentMatrix2);
+        assertEquals("Rentals: John Hood\n" +
+            "4 days of 'Matrix' $5.0\n" +
+            "5 days of 'Matrix2' $15.0\n" +
+            "Total = $20.0\n" +
+            "Frequent renter points = 3\n" +
+            "---\n",
+            customer.statement());
+    }
+    
+    /*---- public void testHtmlCustomer() {
+    	customer.addRental(rentMatrix);
+    	customer.addRental(rentMatrix2);
+    	assertEquals("<html><head><title>Rentals: John Hood</title></head><body>\n"+
+    		     "<h1>Rentals: John Hood</h1>\n"+
+    		     "<table border=1><tr><th>Days</th><th>Title</th><th>Charge</th></tr>\n"+
+    		     "<tr><td align=right>4</td><td>Matrix<td align=right>$5.0</td></tr>\n"+
+    		     "<tr><td align=right>5</td><td>Matrix2<td align=right>$15.0</td></tr>\n"+
+    		     "<tr><td></td><td><i>total</i><td align=right>$20.0</td></tr>\n"+
+    		     "</table><p>Frequent renter points = 3</p>\n"+
+    		     "</body></html>\n",
+    		     customer.htmlStatement());
+        }
+    --- */
+}
