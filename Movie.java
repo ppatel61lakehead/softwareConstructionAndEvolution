a088358f6f865acf105b3ed1d728984f0bde0f7f
Priyank Patel <pbpatel23796@gmail.com>
2020-05-31 20:20:10 -0400

    Step 4 Refactoring
    In this step the method charge() from the Rental.java class has been extracted to Movie.java class

--- Movie.java
+++ Movie.java
@@ -23,4 +23,29 @@ public class Movie {
     public String getTitle() {
         return title;
     }
+    
+    /*
+     * The method charge() has been extracted from Rental.java
+     */
+    public double charge(int daysRented ) {
+		double charge = 0;
+		    	
+    	switch (getPriceCode()) {
+	        case Movie.REGULAR:
+	        	charge += 2;
+	            if (daysRented > 2)
+	            	charge += (daysRented - 2) * 1.5;
+	            break;
+	        case Movie.NEW_RELEASE:
+	        	charge += daysRented * 3;
+	            break;
+	        case Movie.CHILDRENS:
+	        	charge += 1.5;
+	            if (daysRented > 3)
+	            	charge += (daysRented - 3) * 1.5;
+	            break;
+	    }
+    	
+    	return charge;
+    }
 }
